d0180bb6fec3fc7909370f11dc538c3473319899
Luca Macesanu <luca.m@utexas.edu>
2025-11-05 11:05:48 -0500

    Finished tutorial 3: adding a block

--- src/main/java/net/SkipperOnEdge/TutorialMod/TutorialMod.java
+++ src/main/java/net/SkipperOnEdge/TutorialMod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.SkipperOnEdge.TutorialMod;
 
 import com.mojang.logging.LogUtils;
+import net.SkipperOnEdge.TutorialMod.ModBlocks.ModBlocks;
 import net.SkipperOnEdge.TutorialMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -37,6 +38,7 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -55,6 +57,10 @@ public class TutorialMod
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
